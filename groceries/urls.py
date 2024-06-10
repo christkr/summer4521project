efe71f03adef957105a17036c092099100619dd1
christkr <christopher.tkr@gmail.com>
2024-06-09 23:40:10 -0400

    Created home page, login page, add item page, edit item page, delete item page. Created user called superuser for testing. Setup basic login access using superuser.

---
+++ groceries/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('search/', views.search_items, name='search_items'),
+    path('add/', views.add_item, name='add_item'),
+    path('edit/<int:item_id>/', views.edit_item, name='edit_item'),
+    path('delete/<int:item_id>/', views.delete_item, name='delete_item'),
+]
