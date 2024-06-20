19541a5fc90560c85e59a19524f3a409d9ff1e25
Corso Montuori <137323676+Corsionio@users.noreply.github.com>
2024-06-19 20:05:08 -0400

    Add files via upload

--- groceries/views.py
+++ groceries/views.py
@@ -2,23 +2,39 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Item
 from .forms import ItemForm
+from django.db import connections
+
+def get_db_connection(user):
+    if user.groups.filter(name='dbadmin').exists():
+        return 'dbadmin'
+    elif user.groups.filter(name='manager').exists():
+        return 'manager'
+    elif user.groups.filter(name='employee').exists():
+        return 'employee'
+    return 'default'
 
 def home(request):
     return render(request, 'groceries/home.html')
 
 @login_required
 def search_items(request):
+    db_conn = get_db_connection(request.user)
     query = request.GET.get('q')
-    items = Item.objects.filter(name__icontains=query) if query else Item.objects.all()
+    if query:
+        items = Item.objects.using(db_conn).filter(name__icontains=query)
+    else:
+        items = Item.objects.using(db_conn).all()
     return render(request, 'groceries/search_items.html', {'items': items})
 
 @login_required
 @permission_required('groceries.add_item', raise_exception=True)
 def add_item(request):
+    db_conn = get_db_connection(request.user)
     if request.method == 'POST':
         form = ItemForm(request.POST)
         if form.is_valid():
-            form.save()
+            item = form.save(commit=False)
+            item.save(using=db_conn)
             return redirect('search_items')
     else:
         form = ItemForm()
@@ -27,11 +43,13 @@ def add_item(request):
 @login_required
 @permission_required('groceries.change_item', raise_exception=True)
 def edit_item(request, item_id):
-    item = get_object_or_404(Item, id=item_id)
+    db_conn = get_db_connection(request.user)
+    item = get_object_or_404(Item.objects.using(db_conn), id=item_id)
     if request.method == 'POST':
         form = ItemForm(request.POST, instance=item)
         if form.is_valid():
-            form.save()
+            item = form.save(commit=False)
+            item.save(using=db_conn)
             return redirect('search_items')
     else:
         form = ItemForm(instance=item)
@@ -40,8 +58,13 @@ def edit_item(request, item_id):
 @login_required
 @permission_required('groceries.delete_item', raise_exception=True)
 def delete_item(request, item_id):
-    item = get_object_or_404(Item, id=item_id)
+    db_conn = get_db_connection(request.user)
+    item = get_object_or_404(Item.objects.using(db_conn), id=item_id)
     if request.method == 'POST':
-        item.delete()
+        item.delete(using=db_conn)
         return redirect('search_items')
     return render(request, 'groceries/delete_item.html', {'item': item})
+
+def all_items(request):
+    items = Item.objects.all()
+    return render(request, 'groceries/all_items.html', {'items': items})
